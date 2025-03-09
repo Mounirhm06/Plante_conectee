41f950214f9df01ae64d1277b8d99aa218ef409f
HAMMAMI MOUNIR <hmounir707@gmail.com>
2025-03-09 23:03:41 +0100

    Add files via upload

---
+++ Includes/capteur_temperature.h
@@ -0,0 +1,7 @@
+#ifndef CAPTEUR_TEMP
+#define CAPTEUR_TEMP
+	void pin_output_config(void);
+	void pin_input_config(void);
+	void capteur_mesures(uint8_t mesures[]);
+
+#endif
---
+++ Includes/timer.h
@@ -0,0 +1,7 @@
+#ifndef TIMER_H
+#define TIMER_H
+void Timer_initialisation(void);
+void delay_us(int us_delay);
+
+
+#endif
